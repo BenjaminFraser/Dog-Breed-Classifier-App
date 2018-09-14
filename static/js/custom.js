fb7cf68e7e75a6628add765f7cc0ac2284666eb2
BENJAMINFRASER <Ben-fraser@hotmail.co.uk>
2018-09-14 19:49:46 +0100

    Improvements to toggle custom function in custom.js to resize images on toggle.

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,6 +1,9 @@
 function toggle_left_panel() {
     var left_panel = document.getElementById("left_panel");
     var right_panel = document.getElementById("right_panel");
+    var img_gal = document.getElementsByClassName("img-gal");
+
+    console.log(img_gal)
 
     if (left_panel.style.display === "none") {
         left_panel.style.display = "block";
@@ -8,6 +11,10 @@ function toggle_left_panel() {
         right_panel.classList.add('col-md-6');
         right_panel.classList.remove('col-md-12');
 
+        for (var i = 0 ; i < img_gal.length ; i++) {
+            img_gal[i].style.width = "150px";
+            img_gal[i].style.height = "150px";
+        };
 
     } else {
         left_panel.style.display = "none";
@@ -15,6 +22,10 @@ function toggle_left_panel() {
         right_panel.classList.add('col-md-12');
         right_panel.classList.remove('col-md-6');
 
+        for (var i = 0 ; i < img_gal.length ; i++) {
+            img_gal[i].style.width = "250px";
+            img_gal[i].style.height = "250px";
+        };
 
     }
 }
